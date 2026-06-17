b51c457adbb405e6ba0dbf4f670e7866a7ac27e6
mjhervasmozos26 <mjhervasmozos26@gmail.com>
2026-06-17 12:58:02 +0200

    incluir cors

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import express from "express";
+import cors from "cors"; 
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cors()); 
 
 app.get("/", (req, res) => {
     res.json({ mensaje: "Bienvenidos a nuestra API" });
