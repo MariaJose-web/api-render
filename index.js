5fada957df8b7bcc2a1e992546685fb10ebaebed
mjhervasmozos26 <mjhervasmozos26@gmail.com>
2026-06-17 12:14:24 +0200

    segunda version

--- index.js
+++ index.js
@@ -3,12 +3,18 @@ import express from "express";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json()); 
+app.use(express.json());
 
 app.get("/", (req, res) => {
- res.json({mensaje: "Bienvenidos a nuestra API"});
+    res.json({ mensaje: "Bienvenidos a nuestra API" });
 });
 
+app.get("/api", (req, res) => {
+    res.json({
+        nombre: "María José",
+        email: "mjhervasmozos26@gmail.com"
+    });
+});
 app.listen(PORT, () => {
- console.log(`Servidor iniciado en puerto ${PORT}`);
+    console.log(`Servidor iniciado en puerto ${PORT}`);
 });
